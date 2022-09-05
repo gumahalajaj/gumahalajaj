6f11f1f817ef11db1ad88586d592aabfca2bc2c1
Gumah Alajaj <110731357+gumahalajaj@users.noreply.github.com>
2022-09-05 17:55:19 +0300

    gumah

---
+++ src/pages/index.js
@@ -0,0 +1,173 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Box, Section, Image } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { RawHtml, Override, Menu } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section>
+			<Box
+				display="flex"
+				padding="12px 0"
+				justify-content="space-between"
+				align-items="center"
+				flex-direction="row"
+				md-flex-direction="column"
+			>
+				<Text margin="0" md-margin="0px 0 20px 0" text-align="left" font="--lead">
+					Gumah Alajaj
+				</Text>
+				<Menu
+					display="flex"
+					justify-content="center"
+					font="--base"
+					font-weight="700"
+					md-flex-direction="column"
+					md-align-items="center"
+				>
+					<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
+					<Override slot="link-active" color="--primary" />
+					<Override slot="item" padding="6px" />
+				</Menu>
+			</Box>
+		</Section>
+		<Section padding="112px 0 0px 0" background="--color-primary" md-padding="96px 0 0px 0" sm-padding="72px 0 0px 0">
+			<Override slot="SectionContent" flex-direction="row" flex-wrap="wrap" />
+			<Box
+				display="flex"
+				width="50%"
+				flex-direction="column"
+				justify-content="center"
+				align-items="flex-start"
+				lg-width="100%"
+				lg-align-items="center"
+				lg-margin="0px 0px 60px 0px"
+				sm-margin="0px 0px 40px 0px"
+				sm-padding="0px 0px 0px 0px"
+			>
+				<Text
+					margin="0px 0px 24px 0px"
+					color="--light"
+					font="--headline1"
+					lg-text-align="center"
+					sm-font="normal 700 42px/1.2 &quot;Source Sans Pro&quot;, sans-serif"
+					md-font="normal 700 42px/1.2 &quot;Source Sans Pro&quot;, sans-serif"
+				>
+					Gumah ALajaj
+				</Text>
+				<Text
+					margin="0px 0px 40px 0px"
+					color="#FFFFFF"
+					font="--lead"
+					lg-text-align="center"
+					lg-width="80%"
+				>
+					Im Developer Front and Back end Html , css, php, laravel, js, python
+				</Text>
+				<Box
+					display="flex"
+					sm-flex-direction="column"
+					sm-width="100%"
+					sm-text-align="center"
+					justify-content="flex-start"
+					align-items="center"
+				>
+					<Link
+						href="#"
+						padding="12px 24px 12px 24px"
+						color="--dark"
+						background="--color-secondary"
+						text-decoration-line="initial"
+						font="--lead"
+						border-radius="8px"
+						margin="0px 16px 0px 0px"
+						transition="background-color 0.2s ease-in-out 0s"
+						hover-background="--color-orange"
+						sm-margin="0px 0px 16px 0px"
+						sm-text-align="center"
+						sm-width="100%"
+						hover-transition="background-color 0.2s ease-in-out 0s"
+					>
+						Get Started
+					</Link>
+					<Link
+						href="#"
+						padding="10px 24px 10px 24px"
+						color="--secondary"
+						text-decoration-line="initial"
+						font="--lead"
+						border-radius="8px"
+						margin="0px 16px 0px 0px"
+						border-width="2px"
+						border-style="solid"
+						border-color="--color-secondary"
+						sm-margin="0px 0px 0px 0px"
+						sm-text-align="center"
+						lg-margin="0px 0px 0px 0px"
+						sm-width="100%"
+						hover-border-color="--color-orange"
+						hover-color="--orange"
+						hover-transition="color 0.2s ease-in-out 0s,border 0.2s ease-in-out 0s"
+						transition="color 0.2s ease-in-out 0s,border 0.2s ease-in-out 0s"
+					>
+						Check our video
+					</Link>
+				</Box>
+			</Box>
+			<Box
+				display="flex"
+				width="50%"
+				justify-content="center"
+				overflow-y="hidden"
+				overflow-x="hidden"
+				lg-width="100%"
+			>
+				<Image
+					src="https://uploads.quarkly.io/612695d67f2b1f001fa06c1f/images/phone.png?v=2021-08-25T23:23:42.448Z"
+					width="555px"
+					max-width="100%"
+					transform="translateY(10px)"
+					transition="transform 0.5s ease-in-out 0s"
+					hover-transform="translateY(0px)"
+					sm-width="100%"
+				/>
+			</Box>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+		<RawHtml>
+			<style place={"endOfHead"} rawKey={"63160d41fedca4001e35a57c"}>
+				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
+			</style>
+		</RawHtml>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"63160d41fedca4001e35a583",
+				"63160d41fedca4001e35a586"
+			]
+		},
+		"63160d41fedca4001e35a583": {
+			"id": "63160d41fedca4001e35a583",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"63160d41fedca4001e35a586": {
+			"id": "63160d41fedca4001e35a586",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
